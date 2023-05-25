b5254a91f2001c3bcfbd2cb9a14f63d7581decf6
nullxz <81694621+nullxz@users.noreply.github.com>
2023-05-25 18:18:17 +0700

    Update CloudflareDDNS.sh
    แก้ไขตัวแปรให้เป็นพิมพ์ใหญ่เพื่อความง่ายในการอ่าน
    แก้ไขชื่อตัวแปรเล็กน้อยเพื่อความเข้าใจที่ง่ายขึ้น
    เพิ่มข้อความแจ้งข้อผิดพลาดอย่างง่าย

--- CloudflareDDNS.sh
+++ CloudflareDDNS.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #####################################################################################
 # zoneid You can find it on the Overview page. หาได้ในหน้า Overview
-zoneid="" # เช่น 5b0acdf39f4069726d589f4069726d
-name="" # เช่น dynamic.example.org
-comment="" # comment ไม่ต้องใส่ก็ได้
-# Auth API token ตัวอย่าง LlEyQZRx7cFfCy3ab6XLNw42ObnFbfYj
-# Auth API token example LlEyQZRx7cFfCy3ab6XLNw42ObnFbfYj
-auth=""
-ttl="" # 300 = 5 minutes, 3600 = 1hour
-proxy="false" # false / true
+ZONEID="" # เช่น 5b0acdf39f4069726d589f4069726d
+DNS="" # เช่น dynamic.example.org
+COMMENT="" # comment ไม่ต้องใส่ก็ได้
+AUTH="" # Auth API token example LlEyQZRx7cFfCy3ab6XLNw42ObnFbfYj ตัวอย่าง LlEyQZRx7cFfCy3ab6XLNw42ObnFbfYj
+TTL="" # 300 = 5 minutes, 3600 = 1hour
+PROXY="false" # false / true
 #####################################################################################
 #
 #
@@ -19,23 +17,27 @@ proxy="false" # false / true
 #
 #
 # FIND ID
-zonedns=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$name" \
-	-H "Authorization: Bearer $auth" \
+ZONEDNS=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records?type=A&name=$DNS" \
+	-H "Authorization: Bearer $AUTH" \
 	-H "Content-Type: application/json" | grep -Po '"id":"\K[^"]+')
 ## IP
-data=$(curl -s https://api.cloudflare.com/cdn-cgi/trace | grep -Eo 'ip=[0-9.]+')
-ip=$(echo $data | cut -d= -f2)
-echo "The host IP is $ip"
+DATA=$(curl -s https://api.cloudflare.com/cdn-cgi/trace | grep -Eo 'ip=[0-9.]+')
+IP=$(echo $DATA | cut -d= -f2)
+echo "The host IP is $IP"
 ## UPDATE DNS
-curl --request PUT \
-  --url https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$zonedns \
+curl -s --request PUT \
+  --url https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$ZONEDNS \
   --header 'Content-Type: application/json' \
-  --header "Authorization: Bearer $auth" \
+  --header "Authorization: Bearer $AUTH" \
   --data "{
-  \"content\": \"$ip\",
-  \"name\": \"$name\",
-  \"proxied\": $proxy,
+  \"content\": \"$IP\",
+  \"name\": \"$DNS\",
+  \"proxied\": $PROXY,
   \"type\": \"A\",
-  \"comment\": \"$comment\",
-  \"ttl\": $ttl
-}"
+  \"comment\": \"$COMMENT\",
+  \"ttl\": $TTL
+}" | if grep -q '"success":true'; then 
+	echo "Success" 
+ else 
+ 	echo "Error BAD AUTH or ZONEID" 
+ fi
