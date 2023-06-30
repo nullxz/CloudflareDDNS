265aaf3a2b49ec3c098be4f9c0f04014e2327617
nullxz <81694621+nullxz@users.noreply.github.com>
2023-06-30 21:36:37 +0700

    Update CloudflareDDNS.sh
    ทำให้ output ดูดีขึ้นอ่านง่ายขึ้น
    แก้ตัวอย่างเล็กน้อย

--- CloudflareDDNS.sh
+++ CloudflareDDNS.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 #####################################################################################
 # zoneid You can find it on the Overview page. หาได้ในหน้า Overview
+# DNS "A" Only / IPv4
 ZONEID="" # เช่น 5b0acdf39f4069726d589f4069726d
 DNS="" # เช่น dynamic.example.org
 COMMENT="" # comment ไม่ต้องใส่ก็ได้
-AUTH="" # Auth API token example LlEyQZRx7cFfCy3ab6XLNw42ObnFbfYj ตัวอย่าง LlEyQZRx7cFfCy3ab6XLNw42ObnFbfYj
+AUTH="" # Auth API token example V1NQQ1VoYTBaMDJGXzA0cUthS290NW5rdlJleW40bHUK ตัวอย่าง V1NQQ1VoYTBaMDJGXzA0cUthS290NW5rdlJleW40bHUK
 TTL="" # 300 = 5 minutes, 3600 = 1hour
 PROXY="false" # false / true
 #####################################################################################
-#
-#
-#
-#
-#
-#
-#
-#
+echo "========================================================"
+date -R
 # FIND ID
 ZONEDNS=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records?type=A&name=$DNS" \
 	-H "Authorization: Bearer $AUTH" \
@@ -25,7 +20,7 @@ DATA=$(curl -s https://api.cloudflare.com/cdn-cgi/trace | grep -Eo 'ip=[0-9.]+')
 IP=$(echo $DATA | cut -d= -f2)
 echo "The host IP is $IP"
 ## UPDATE DNS
-curl -s --request PUT \
+RESPONSE=$(curl -s --request PUT \
   --url https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$ZONEDNS \
   --header 'Content-Type: application/json' \
   --header "Authorization: Bearer $AUTH" \
@@ -36,8 +31,14 @@ curl -s --request PUT \
   \"type\": \"A\",
   \"comment\": \"$COMMENT\",
   \"ttl\": $TTL
-}" | if grep -q '"success":true'; then 
-	echo "Success" 
- else 
- 	echo "Error BAD AUTH or ZONEID" 
- fi
+}")
+SUCCESS=$(echo "$RESPONSE" | grep -o '"success":true')
+if [[ "$SUCCESS" == "\"success\":true" ]]; then
+  echo "$RESPONSE"
+  echo "[ OK ]"
+  echo "========================================================"
+
+else
+  echo "[ fail ] BAD AUTH or ZONEID"
+  echo "========================================================"
+fi
