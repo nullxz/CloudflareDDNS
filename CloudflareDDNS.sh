5d5b026d0acfad1b64569f3d098f97514fb16229
nullxz <nusara@furnu.org>
2024-01-01 03:46:58 +0700

    Update CloudflareDDNS.sh
    Add IPv6 Support

--- CloudflareDDNS.sh
+++ CloudflareDDNS.sh
@@ -1,44 +1,62 @@
-#!/bin/bash
-#####################################################################################
-# zoneid You can find it on the Overview page. หาได้ในหน้า Overview
-# DNS "A" Only / IPv4
-ZONEID="" # เช่น 5b0acdf39f4069726d589f4069726d
-DNS="" # เช่น dynamic.example.org
-COMMENT="" # comment ไม่ต้องใส่ก็ได้
-AUTH="" # Auth API token example V1NQQ1VoYTBaMDJGXzA0cUthS290NW5rdlJleW40bHUK ตัวอย่าง V1NQQ1VoYTBaMDJGXzA0cUthS290NW5rdlJleW40bHUK
-TTL="" # 300 = 5 minutes, 3600 = 1hour
-PROXY="false" # false / true
-#####################################################################################
-echo "========================================================"
-date -R
+#!/bin/sh
+# dynamic-address.example.com
+DNS=""
+# 60 = 1m, 300 = 5m, 3600 = 1h
+TTL=""
+# My Home Server
+COMMENT=""
+# "4" OR "6" Only
+IPv=""
+# 764efa883dda1e11db47671c4a3bbd9e
+ZONEID=""
+# NzY0ZWZhODgzZGRhMWUxMWRiNDc2NzFjNGEzYmJkOWUK
+TOKEN=""
+# true OR false
+PROXY="false"
+
+
+
+
 # FIND ID
 ZONEDNS=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records?type=A&name=$DNS" \
-	-H "Authorization: Bearer $AUTH" \
+	-H "Authorization: Bearer $TOKEN" \
 	-H "Content-Type: application/json" | grep -Po '"id":"\K[^"]+')
-## IP
-DATA=$(curl -s https://api.cloudflare.com/cdn-cgi/trace | grep -Eo 'ip=[0-9.]+')
-IP=$(echo $DATA | cut -d= -f2)
-echo "The host IP is $IP"
-## UPDATE DNS
+
+case $IPv in
+	4)
+		DATA=$(curl -s https://api.cloudflare.com/cdn-cgi/trace | awk -F= '/ip=([0-9.]+)/ {print $2}')
+		TYPE="A"
+	;;
+	6)
+		DATA=$(curl -s https://api.cloudflare.com/cdn-cgi/trace | awk -F= '/ip=([0-9a-fA-F:]+)/ {print $2}')
+		TYPE="AAAA"
+	;;
+	*)
+		echo "ERROR: Allow Only IPv4 OR IPv6"
+		exit 1
+	;;
+esac
+
+echo "The host IP is $DATA"
+
 RESPONSE=$(curl -s --request PUT \
   --url https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$ZONEDNS \
   --header 'Content-Type: application/json' \
-  --header "Authorization: Bearer $AUTH" \
+  --header "Authorization: Bearer $TOKEN" \
   --data "{
-  \"content\": \"$IP\",
+  \"content\": \"$DATA\",
   \"name\": \"$DNS\",
   \"proxied\": $PROXY,
-  \"type\": \"A\",
+  \"type\": \"$TYPE\",
   \"comment\": \"$COMMENT\",
   \"ttl\": $TTL
 }")
+
 SUCCESS=$(echo "$RESPONSE" | grep -o '"success":true')
 if [[ "$SUCCESS" == "\"success\":true" ]]; then
-  echo "$RESPONSE"
-  echo "[ OK ]"
-  echo "========================================================"
-
+	echo "$RESPONSE"
+	echo "[ OK ]"
 else
-  echo "[ fail ] BAD AUTH or ZONEID"
-  echo "========================================================"
+	echo "$RESPONSE"
+	echo "[ FAIL ] BAD AUTH OR ZONEID"
 fi
